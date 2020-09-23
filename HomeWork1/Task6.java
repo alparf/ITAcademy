5cff785667b8c247332a0cacc17eddc550b59b87
alparf <a.l.parfiankou@gmail.com>
2020-09-23 14:56:50 +0300

    mac

---
+++ HomeWork1/Task6.java
@@ -0,0 +1,19 @@
+package HomeWork1;
+
+public class Task6 {
+    public static String createPhoneNumber(int[] numbs) {
+        StringBuilder sb = new StringBuilder();
+        for(int numb: numbs) {
+            sb.append(numb);
+        }
+        sb.insert(0,"(");
+        sb.insert(4,") ");
+        sb.insert(9, "-");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] phoneNumber = {1,2,3,4,5,6,7,8,9,0};
+        System.out.println(createPhoneNumber(phoneNumber));
+    }
+}
