45dc9232980a2fff2e6a6516cf061fa6731a7e50
alparf <a.l.parfiankou@gmail.com>
2020-10-06 23:49:21 +0300

    start sorting

---
+++ HomeWork2/Task4/Runner.java
@@ -0,0 +1,7 @@
+package HomeWork2.Task4;
+
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ HomeWork2/Task4/Sorting.java
@@ -0,0 +1,11 @@
+package HomeWork2.Task4;
+
+public class Sorting {
+    public static double[] bubbleSort(double[] array) {
+        return null;
+    }
+
+    public static double[] cocktailSort(double[] array) {
+        return null;
+    }
+}
