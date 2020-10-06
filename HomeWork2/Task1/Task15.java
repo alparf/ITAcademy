47b4ce12d4fe52143a0692fb2b90af961597727d
alparf <a.l.parfiankou@gmail.com>
2020-10-06 16:12:58 +0300

    commint

--- HomeWork2/Task1/Task15.java
+++ HomeWork2/Task1/Task15.java
@@ -2,8 +2,8 @@ package HomeWork2.Task1;
 
 public class Task15 {
     private static void print(int x, int y) {
-        for(int i = 1; i < 11; i++) {
-            for(int j = x; j < y; j++) {
+        for (int i = 1; i < 11; i++) {
+            for (int j = x; j < y; j++) {
                 System.out.print(j + " * " + i + " = " + i * j + "\t\t");
             }
             System.out.println();
---
+++ HomeWork2/Task2/Task21.java
@@ -0,0 +1,46 @@
+package HomeWork2.Task2;
+
+import java.util.Scanner;
+
+public class Task21 {
+    private static final String SPACE = " ";
+    private static final String EXTRA_SPACE = "  ";
+
+    private static String formatLine(String line) {
+        while (line.contains(EXTRA_SPACE)) {
+            String replaceLine = line.replace(EXTRA_SPACE, SPACE);
+            line = replaceLine;
+        }
+        return line.trim();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numbsLine = formatLine(scanner.nextLine());
+        String[] lineArray = numbsLine.split(SPACE);
+        double[] numbs = new double[lineArray.length];
+        for (int i = 0; i < lineArray.length; i++) {
+            numbs[i] = Double.valueOf(lineArray[i]);
+        }
+        System.out.println("Print do ...while:");
+        int index = 0;
+        do {
+            System.out.println(numbs[index]);
+            index++;
+        } while (index < numbs.length);
+        System.out.println("\nPrint while:");
+        index = 0;
+        while (index < numbs.length) {
+            System.out.println(numbs[index]);
+            index++;
+        }
+        System.out.println("\nPrint for:");
+        for (int i = 0; i < numbs.length; i++) {
+            System.out.println(numbs[i]);
+        }
+        System.out.println("\nPrint foreach:");
+        for (double numb: numbs) {
+            System.out.println(numb);
+        }
+    }
+}
