65a65a232939f4144d2c98173ead81e1c7f5ec0f
alparf <a.l.parfiankou@gmail.com>
2020-10-06 14:02:37 +0300

    commint

---
+++ HomeWork2/Task1/Task15.java
@@ -0,0 +1,18 @@
+package HomeWork2.Task1;
+
+public class Task15 {
+    private static void print(int x, int y) {
+        for(int i = 1; i < 11; i++) {
+            for(int j = x; j < y; j++) {
+                System.out.print(j + " * " + i + " = " + i * j + "\t\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(2,6);
+        System.out.println();
+        print(6,10);
+    }
+}
