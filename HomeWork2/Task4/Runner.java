d6d80758d5a077fb5c808533628d9b8a067bd139
alparf <a.l.parfiankou@gmail.com>
2020-10-07 00:07:46 +0300

    start test

--- HomeWork2/Task4/Runner.java
+++ HomeWork2/Task4/Runner.java
@@ -1,7 +1,30 @@
 package HomeWork2.Task4;
 
-public class Runner {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Runner {
+    private static final double[] FIRST_TEST = {1, 2, 3, 4, 5, 6};
+    private static final double[] SECOND_TEST = {1, 1, 1, 1,};
+    private static final double[] THIRD_TEST = {9, 1, 5, 99, 9, 9};
+    private static final double[] FINAL_TEST = {};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(FIRST_TEST));
+        System.out.println(Arrays.toString(Sort.bubbleSort(FIRST_TEST)));
+        System.out.println(Arrays.toString(SECOND_TEST));
+        System.out.println(Arrays.toString(Sort.bubbleSort(SECOND_TEST)));
+        System.out.println(Arrays.toString(THIRD_TEST));
+        System.out.println(Arrays.toString(Sort.bubbleSort(THIRD_TEST)));
+        System.out.println(Arrays.toString(FINAL_TEST));
+        System.out.println(Arrays.toString(Sort.bubbleSort(FINAL_TEST)));
+
+        System.out.println(Arrays.toString(FIRST_TEST));
+        System.out.println(Arrays.toString(Sort.cocktailSort(FIRST_TEST)));
+        System.out.println(Arrays.toString(SECOND_TEST));
+        System.out.println(Arrays.toString(Sort.cocktailSort(SECOND_TEST)));
+        System.out.println(Arrays.toString(THIRD_TEST));
+        System.out.println(Arrays.toString(Sort.cocktailSort(THIRD_TEST)));
+        System.out.println(Arrays.toString(FINAL_TEST));
+        System.out.println(Arrays.toString(Sort.cocktailSort(FINAL_TEST)));
     }
 }
--- HomeWork2/Task4/Sort.java
+++ HomeWork2/Task4/Sort.java
@@ -1,11 +1,15 @@
 package HomeWork2.Task4;
 
-public class Sorting {
+public class Sort {
     public static double[] bubbleSort(double[] array) {
-        return null;
+        int size = array.length;
+        for(int i = 0; i < size; i++) {
+
+        }
+        return array;
     }
 
     public static double[] cocktailSort(double[] array) {
-        return null;
+        return array;
     }
 }
