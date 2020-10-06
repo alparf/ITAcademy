e0040f87af42cdbcec352972a2e494b1a8779926
alparf <a.l.parfiankou@gmail.com>
2020-10-07 00:56:20 +0300

    Bobble sort test

--- HomeWork2/Task4/Runner.java
+++ HomeWork2/Task4/Runner.java
@@ -3,28 +3,30 @@ package HomeWork2.Task4;
 import java.util.Arrays;
 
 public class Runner {
-    private static final double[] FIRST_TEST = {1, 2, 3, 4, 5, 6};
-    private static final double[] SECOND_TEST = {1, 1, 1, 1,};
-    private static final double[] THIRD_TEST = {9, 1, 5, 99, 9, 9};
-    private static final double[] FINAL_TEST = {};
+    private static double[] firstTest = {1, 2, 3, 4, 5, 6};
+    private static double[] secondTest = {1, 1, 1, 1,};
+    private static double[] thirdTest = {9, 1, 5, 99, 9, 9};
+    private static double[] finalTest = {};
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(FIRST_TEST));
-        System.out.println(Arrays.toString(Sort.bubbleSort(FIRST_TEST)));
-        System.out.println(Arrays.toString(SECOND_TEST));
-        System.out.println(Arrays.toString(Sort.bubbleSort(SECOND_TEST)));
-        System.out.println(Arrays.toString(THIRD_TEST));
-        System.out.println(Arrays.toString(Sort.bubbleSort(THIRD_TEST)));
-        System.out.println(Arrays.toString(FINAL_TEST));
-        System.out.println(Arrays.toString(Sort.bubbleSort(FINAL_TEST)));
+        System.out.println("Bobble Sort: ");
+        System.out.print(Arrays.toString(firstTest) + " => ");
+        System.out.println(Arrays.toString(Sort.bubbleSort(firstTest)));
+        System.out.print(Arrays.toString(secondTest) + " => ");
+        System.out.println(Arrays.toString(Sort.bubbleSort(secondTest)));
+        System.out.print(Arrays.toString(thirdTest) + " => ");
+        System.out.println(Arrays.toString(Sort.bubbleSort(thirdTest)));
+        System.out.print(Arrays.toString(finalTest) + " => ");
+        System.out.println(Arrays.toString(Sort.bubbleSort(finalTest)));
 
-        System.out.println(Arrays.toString(FIRST_TEST));
-        System.out.println(Arrays.toString(Sort.cocktailSort(FIRST_TEST)));
-        System.out.println(Arrays.toString(SECOND_TEST));
-        System.out.println(Arrays.toString(Sort.cocktailSort(SECOND_TEST)));
-        System.out.println(Arrays.toString(THIRD_TEST));
-        System.out.println(Arrays.toString(Sort.cocktailSort(THIRD_TEST)));
-        System.out.println(Arrays.toString(FINAL_TEST));
-        System.out.println(Arrays.toString(Sort.cocktailSort(FINAL_TEST)));
+        System.out.println("Cocktail Sort: ");
+        System.out.print(Arrays.toString(firstTest) + " => ");
+        System.out.println(Arrays.toString(Sort.cocktailSort(firstTest)));
+        System.out.print(Arrays.toString(secondTest) + " => ");
+        System.out.println(Arrays.toString(Sort.cocktailSort(secondTest)));
+        System.out.print(Arrays.toString(thirdTest) + " => ");
+        System.out.println(Arrays.toString(Sort.cocktailSort(thirdTest)));
+        System.out.print(Arrays.toString(finalTest) + " => ");
+        System.out.println(Arrays.toString(Sort.cocktailSort(finalTest)));
     }
 }
--- HomeWork2/Task4/Sort.java
+++ HomeWork2/Task4/Sort.java
@@ -1,15 +1,34 @@
 package HomeWork2.Task4;
 
+import java.util.Arrays;
+
 public class Sort {
+
     public static double[] bubbleSort(double[] array) {
         int size = array.length;
-        for(int i = 0; i < size; i++) {
-
+        double[] sortedArray = Arrays.copyOf(array, size);
+        double buffer;
+        boolean isChange;
+        for( int n = 1; n < size; n ++) {
+            isChange = false;
+            for (int i = 0; i < size - 1; i++) {
+                if (sortedArray[i] > sortedArray[i + 1]) {
+                    buffer = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = buffer;
+                    isChange = true;
+                }
+            }
+            if (!isChange) {
+                break;
+            }
         }
-        return array;
+        return sortedArray;
     }
 
     public static double[] cocktailSort(double[] array) {
-        return array;
+        int size = array.length;
+        double[] sortedArray = Arrays.copyOf(array, size);
+        return sortedArray;
     }
 }
