30dfe70fdb5c2c7153c50f7dafa62264a4061257
alparf <a.l.parfiankou@gmail.com>
2020-10-10 15:48:43 +0300

    1. completed

---
+++ HomeWork3/Runner.java
@@ -0,0 +1,7 @@
+package HomeWork3;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(new WithOutCalculatorMain().getResult());
+    }
+}
---
+++ HomeWork3/WithOutCalculatorMain.java
@@ -0,0 +1,13 @@
+package HomeWork3;
+
+public class WithOutCalculatorMain {
+    private double result;
+
+    public WithOutCalculatorMain() {
+        result = (4.1 + 15 * 7 + (28.0 / 5)) * (4.1 + 15 * 7 + (28.0 / 5));
+    }
+
+    public double getResult() {
+        return result;
+    }
+}
