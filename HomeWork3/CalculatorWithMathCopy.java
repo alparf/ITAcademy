3a3b828d6652fbc6ad6648c1f6858df9bd255602
alparf <a.l.parfiankou@gmail.com>
2020-10-10 18:23:03 +0300

    commit CalculatorWithMathCopy

---
+++ HomeWork3/CalculatorWithMathCopy.java
@@ -0,0 +1,32 @@
+package HomeWork3;
+
+public class CalculatorWithMathCopy {
+
+    public double summation(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+
+    public double raiseTo(double a, int b) {
+        return Math.pow(a, b);
+    }
+
+    public double absolution(double a) {
+        return Math.abs(a);
+    }
+
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+}
---
+++ HomeWork3/CalculatorWithMathCopyMain.java
@@ -0,0 +1,12 @@
+package HomeWork3;
+
+public class CalculatorWithMathCopyMain {
+
+    public static void main(String[] args) {
+        CalculatorWithMathCopy calculator = new CalculatorWithMathCopy();
+        double result = calculator.summation(
+                calculator.summation(4.1, calculator.multiplication(15, 7)),
+                calculator.raiseTo(calculator.division(28, 5),2));
+        System.out.println(result);
+    }
+}
