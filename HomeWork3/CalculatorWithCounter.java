d190ac52a910c872b81f52b32259dfd660a66cba
alparf <a.l.parfiankou@gmail.com>
2020-10-10 22:14:16 +0300

    -abstract

--- HomeWork3/CalculatorWithCounter.java
+++ HomeWork3/CalculatorWithCounter.java
@@ -29,63 +29,56 @@ public class CalculatorWithCounter {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.summation(a, b);
-        } else {
-            return mathCalculator.summation(a, b);
         }
+        return mathCalculator.summation(a, b);
     }
 
     public double subtraction(double a, double b) {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.subtraction(a, b);
-        } else {
-            return mathCalculator.subtraction(a, b);
         }
+        return mathCalculator.subtraction(a, b);
     }
 
     public double multiplication(double a, double b) {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.multiplication(a, b);
-        } else {
-            return mathCalculator.multiplication(a, b);
         }
+        return mathCalculator.multiplication(a, b);
     }
 
     public double division(double a, double b) {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.division(a, b);
-        } else {
-            return mathCalculator.division(a, b);
         }
+        return mathCalculator.division(a, b);
     }
 
     public double raiseTo(double a, int b) {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.raiseTo(a, b);
-        } else {
-            return mathCalculator.raiseTo(a, b);
         }
+        return mathCalculator.raiseTo(a, b);
     }
 
     public double absolute(double a) {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.absolute(a);
-        } else {
-            return mathCalculator.absolute(a);
         }
+        return mathCalculator.absolute(a);
     }
 
     public double squareRoot(double a) {
         this.countOperation++;
         if (isUserInit) {
             return userCalculator.squareRoot(a);
-        } else {
-            return mathCalculator.squareRoot(a);
         }
+        return mathCalculator.squareRoot(a);
     }
 
 }
