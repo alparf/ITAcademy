4b5d7b9e6f9043559748254502f1f5802836ea33
alparf <a.l.parfiankou@gmail.com>
2020-10-10 17:45:55 +0300

    fix more errors :)

--- HomeWork3/CalculatorWithOperator.java
+++ HomeWork3/CalculatorWithOperator.java
@@ -1,6 +1,7 @@
 package HomeWork3;
 
 public class CalculatorWithOperator {
+
     public double summation(double a, double b) {
         return a + b;
     }
@@ -17,15 +18,22 @@ public class CalculatorWithOperator {
         return a / b;
     }
 
+    /**
+     *
+     * @param a first argument (any positive number)
+     * @param b second argument (any integer)
+     * @return the value of the first argument raised to the power of the second argument
+     */
+
     public double raisedTo(double a, int b) {
         if (b == 0) {
             return 1;
         } else {
-            for (int i = 1; i < b; i++) {
-                a *= a;
+            for (int i = 1; i < this.absolute(b); i++) {
+                    a *= a;
             }
         }
-        return a;
+        return b > 0 ? a : 1 / a;
     }
 
     public double absolute(double a) {
