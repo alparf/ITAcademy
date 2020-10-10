100a372c1c4de1906b9a674496f6393d57156220
alparf <a.l.parfiankou@gmail.com>
2020-10-10 16:04:46 +0300

    1. rework

--- HomeWork3/Runner.java
+++ HomeWork3/Runner.java
@@ -2,6 +2,5 @@ package HomeWork3;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println(WithOutCalculatorMain.RESULT);
     }
 }
--- HomeWork3/WithOutCalculatorMain.java
+++ HomeWork3/WithOutCalculatorMain.java
@@ -2,4 +2,8 @@ package HomeWork3;
 
 public abstract class WithOutCalculatorMain {
     public static final double RESULT = (4.1 + 15 * 7 + (28.0 / 5)) * (4.1 + 15 * 7 + (28.0 / 5));
+
+    public static void main(String[] args) {
+        System.out.println(WithOutCalculatorMain.RESULT);
+    }
 }
