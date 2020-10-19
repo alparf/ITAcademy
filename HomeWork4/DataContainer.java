9ebcaf36f157b82d6292e265dd5e268fc4cac462
alparf <a.l.parfiankou@gmail.com>
2020-10-20 00:35:00 +0300

    task 1-4

---
+++ HomeWork4/DataContainer.java
@@ -0,0 +1,85 @@
+package HomeWork4;
+
+import java.util.Arrays;
+
+public class DataContainer<T> {
+    private T[] data;
+    private int size;
+    private int capacity;
+
+    private void increaseCapacity() {
+        int newCapacity = this.size * 2;
+        T[] newArray = Arrays.copyOf(this.data, newCapacity);
+        this.data = newArray;
+        this.capacity = newCapacity;
+    }
+
+    private int getFirstIndexOfNull() {
+        for (int i = 0; i < this.size; i++) {
+            if(null == this.data[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public DataContainer(T[] data) {
+        this.data = data;
+        if(null != data) {
+            this.size = data.length;
+            this.capacity = this.size;
+        }
+    }
+
+    public int add(T item) {
+        if(null == item) {
+            return -1;
+        }
+        int nullIndex = getFirstIndexOfNull();
+        if(nullIndex >= 0) {
+            this.data[nullIndex] = item;
+            return nullIndex;
+        }
+        if(!(this.size < this.capacity)) {
+            this.increaseCapacity();
+        }
+        this.data[size++] = item;
+        return this.size;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public T[] getData() {
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        boolean isPut = false;
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < this.size; i++) {
+            if(null != this.data[i]) {
+                if(isPut) {
+                    sb.append(", ");
+                } else {
+                    isPut = true;
+                }
+                sb.append(this.data[i].toString());
+            } else {
+                if(isPut) {
+                    sb.append(", ");
+                }
+                sb.append("null");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ HomeWork4/Runner.java
@@ -0,0 +1,21 @@
+package HomeWork4;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("sample №1: ");
+        DataContainer<Integer> test = new DataContainer<>(new Integer[] {1, 2, 3, null, null, null});
+        System.out.println(test);
+        System.out.println("add(777) index: " + test.add(777));
+        System.out.println(test);
+        System.out.println("\nsample №2: ");
+        test = new DataContainer<>(new Integer[] {1, 2, 3, null, null, null});
+        System.out.println(test);
+        System.out.println("add(null) index: " + test.add(null));
+        System.out.println(test);
+        System.out.println("\nsample №3: ");
+        test = new DataContainer<>(new Integer[] {1, null, 3, null, null, null});
+        System.out.println(test);
+        System.out.println("add(777) index: " + test.add(777));
+        System.out.println(test);
+    }
+}
