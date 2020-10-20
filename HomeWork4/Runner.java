c7e879fb8deec44b014ca4ce9e2543ac38919797
alparf <a.l.parfiankou@gmail.com>
2020-10-20 13:54:44 +0300

    main fixe

--- HomeWork4/Runner.java
+++ HomeWork4/Runner.java
@@ -88,9 +88,6 @@ public class Runner {
             }
         });
         System.out.println(exampleStatic);
-        example = new DataContainer<>(null);
-        example.add(2);
-        System.out.println(example);
     }
 }
 
