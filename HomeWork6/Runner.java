16d2f657d296e9efbc0664e0452966839448d818
alparf <a.l.parfiankou@gmail.com>
2020-11-23 11:35:13 +0300

    commit

--- HomeWork6/Runner.java
+++ HomeWork6/Runner.java
@@ -1,14 +1,29 @@
 package HomeWork6;
 
+import HomeWork6.Interface.ISearchEngine;
 import HomeWork6.Search.EasySearch;
 import HomeWork6.Search.RegExSearch;
 
 import static HomeWork6.Constants.FileConstant.*;
 
 import java.io.*;
-import java.util.concurrent.Callable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Runner {
+    private static long getCounter(List<Future<Long>> futures) throws ExecutionException, InterruptedException {
+        long counter = 0;
+        Iterator<Future<Long>> itr = futures.iterator();
+        while (!futures.isEmpty()) {
+            while (itr.hasNext()) {
+                counter += itr.next().get();
+                itr.remove();
+            }
+        }
+        return counter;
+    }
 
     public static void main(String[] args) {
         String absolutePath = new File("").getAbsolutePath();
@@ -18,14 +33,14 @@ public class Runner {
         final int WAR = 0, AND = 1, PEACE = 2;
         final int parts = 4;
         String[] text = TxtFileReader.fileToStringArray(file, parts);
-        Thread[] threads = new Thread[parts];
+        final ISearchEngine easySearch = new EasySearch();
         long counter = 0;
         //Лямбдой
         try {
             for(int i = 0; i < parts; i++) {
                 int index = i;
                 Callable<Long> callable = () -> {
-                    return new EasySearch().search(text[index], LOOKING_FOR[WAR]);
+                    return easySearch.search(text[index], LOOKING_FOR[WAR]);
                 };
                 counter += callable.call();
             }
@@ -36,10 +51,11 @@ public class Runner {
         System.out.println(LOOKING_FOR[WAR] + " = " + counter);
         //Обычным классом
         counter = 0;
+        final ISearchEngine regExSearch = new RegExSearch();
         try {
             SearchTask[] tasks = new SearchTask[parts];
             for(int i = 0; i < parts; i++) {
-                tasks[i] = new SearchTask(new RegExSearch(), text[i], LOOKING_FOR[WAR]);
+                tasks[i] = new SearchTask(regExSearch, text[i], LOOKING_FOR[WAR]);
                 counter += tasks[i].call();
             }
         } catch (Exception e) {
@@ -47,5 +63,26 @@ public class Runner {
         }
         System.out.println("\nRegExSearch:");
         System.out.println(LOOKING_FOR[WAR] + " = " + counter);
+        //ExecutorService
+        System.out.println("\nExecutorService:");
+        List<Future<Long>> warFutures = new LinkedList<>();
+        List<Future<Long>> andFutures = new LinkedList<>();
+        List<Future<Long>> peaceFutures = new LinkedList<>();
+        ExecutorService service = Executors.newFixedThreadPool(parts);
+        try {
+            for(int i = 0; i < parts; i++) {
+                warFutures.add(service.submit(new SearchTask(regExSearch, text[i].toLowerCase(), LOOKING_FOR[WAR])));
+                andFutures.add(service.submit(new SearchTask(regExSearch, text[i].toLowerCase(), LOOKING_FOR[AND])));
+                peaceFutures.add(service.submit(new SearchTask(regExSearch, text[i].toLowerCase(), LOOKING_FOR[PEACE])));
+            }
+            System.out.println(LOOKING_FOR[WAR] + " = " + getCounter(warFutures));
+            System.out.println(LOOKING_FOR[AND] + " = " + getCounter(andFutures));
+            System.out.println(LOOKING_FOR[PEACE] + " = " + getCounter(peaceFutures));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        service.shutdown();
     }
 }
