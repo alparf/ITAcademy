ed677861fd609eec2c241543da1e114405ebc327
alparf <a.l.parfiankou@gmail.com>
2020-11-27 14:56:21 +0300

    commit

--- HomeWork7/Test.java
+++ HomeWork7/Test.java
@@ -1,13 +1,49 @@
 package HomeWork7;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
         printRates(new NBRBLoader());
     }
 
     public static void printRates(SiteLoader loader){
-        System.out.println(loader.load(SiteLoader.Currency.EUR));
-        System.out.println(loader.load(SiteLoader.Currency.RUB));
-        System.out.println(loader.load(SiteLoader.Currency.USD));
+        final String FILE_NAME = "Currency.txt";
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter file path to save data or press \"Enter to save in root");
+            System.out.print("File path: ");
+            String filePath = scanner.nextLine();
+            File file;
+            if(filePath.isEmpty()) {
+                file = new File(FILE_NAME);
+            } else {
+                file = new File(filePath, FILE_NAME);
+            }
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            if(file.exists() && file.isFile()) {
+                try (FileWriter fileWriter = new FileWriter(file)) {
+                    fileWriter
+                            .append(SiteLoader.Currency.EUR + " : ")
+                            .append(String.valueOf(loader.load(SiteLoader.Currency.EUR)))
+                            .append(System.lineSeparator())
+                            .append(SiteLoader.Currency.RUB + " : ")
+                            .append(String.valueOf(loader.load(SiteLoader.Currency.RUB)))
+                            .append(System.lineSeparator())
+                            .append(SiteLoader.Currency.USD + " : ")
+                            .append(String.valueOf(loader.load(SiteLoader.Currency.USD)))
+                            .append(System.lineSeparator());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(SiteLoader.Currency.EUR + " : " + loader.load(SiteLoader.Currency.EUR));
+        System.out.println(SiteLoader.Currency.RUB + " : " + loader.load(SiteLoader.Currency.RUB));
+        System.out.println(SiteLoader.Currency.USD + " : " + loader.load(SiteLoader.Currency.USD));
     }
 }
