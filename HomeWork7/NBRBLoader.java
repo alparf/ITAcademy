ee45166241efcfd25417d7d324c37dbabc597b9f
alparf <a.l.parfiankou@gmail.com>
2020-11-27 12:30:54 +0300

    commit

---
+++ HomeWork7/NBRBLoader.java
@@ -0,0 +1,29 @@
+package HomeWork7;
+
+/**
+ * Загрузчик курса с сайта Нац. Банка
+ */
+public class NBRBLoader extends SiteLoader {
+
+    /**
+     * Метод для запуска загрузки курса валют
+     * @param currencyName валюта которую мы ищем
+     * @return курс который мы нашли
+     */
+    @Override
+    public double load(SiteLoader.Currency currencyName) {
+        return load("https://www.nbrb.by/api/exrates/rates/" + currencyName.getId(), currencyName);
+    }
+
+    /**
+     * Обработка результата загрузки с сайта банка
+     * @param content то что получилось загрузить
+     * @param currencyName валюта которую мы ищем
+     * @return курс который мы нашли
+     */
+    @Override
+    protected double handle(String content, SiteLoader.Currency currencyName) {
+        //TODO дописываем код сюда
+        return 0;
+    }
+}
