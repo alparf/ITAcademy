e898cc292d08104b7e5422b04f4564ea7097e79a
alparf <a.l.parfiankou@gmail.com>
2020-12-01 23:21:27 +0300

    commit

--- HomeWork7/Test.java
+++ HomeWork7/Test.java
@@ -5,55 +5,60 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        saveRates(Loaders.newABLoader());
+        printRate(Loaders.newABLoader());
     }
 
-    public static void saveRates(SiteLoader loader){
-        String currencyEur = SiteLoader.Currency.EUR + " : " + loader.load(SiteLoader.Currency.EUR);
-        String currencyRub = SiteLoader.Currency.RUB + " : " + loader.load(SiteLoader.Currency.RUB);
-        String currencyUsd = SiteLoader.Currency.USD + " : " + loader.load(SiteLoader.Currency.USD);
-        System.out.println(currencyEur);
-        System.out.println(currencyRub);
-        System.out.println(currencyUsd);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("[dd.MM.yyyy]");
-        String currentDate = simpleDateFormat.format(new Date());
-        final String FILE_NAME = "Currency.txt";
+    public static void printRate(SiteLoader loader){
+        List<String> rates = new LinkedList<>();
+        rates.add(SiteLoader.Currency.EUR + " : " + loader.load(SiteLoader.Currency.EUR));
+        rates.add(SiteLoader.Currency.RUB + " : " + loader.load(SiteLoader.Currency.RUB));
+        rates.add(SiteLoader.Currency.USD + " : " + loader.load(SiteLoader.Currency.USD));
+        for(String rate: rates) {
+            System.out.println(rate);
+        }
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter file path to save data or press \"Enter to save in root");
             System.out.print("File path: ");
             String filePath = scanner.nextLine();
-            File file;
-            if(filePath.isEmpty()) {
-                file = new File(FILE_NAME);
-            } else {
-                file = new File(filePath, FILE_NAME);
-            }
-            if(!file.exists()) {
-                try {
-                    file.createNewFile();
-                } catch (IOException e) {
-                    throw new IllegalArgumentException(filePath);
-                }
-            }
-            if(file.exists() && file.isFile()) {
-                try (FileWriter fileWriter = new FileWriter(file, true)) {
-                    fileWriter
-                            .append(currentDate + " ").append(currencyEur)
-                            .append(System.lineSeparator())
-                            .append(currentDate + " ").append(currencyRub)
-                            .append(System.lineSeparator())
-                            .append(currentDate + " ").append(currencyUsd)
-                            .append(System.lineSeparator());
-                }
-            }
+            saveRate(filePath, rates);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (IllegalArgumentException e) {
             System.out.println("Invalid file path!");
+        }
+    }
+
+    private static void saveRate(String filePath, List<String> lines) throws IOException {
+        final String FILE_NAME = "Currency.txt";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("[dd.MM.yyyy]");
+        String currentDate = simpleDateFormat.format(new Date());
+        File file;
+        if(filePath.isEmpty()) {
+            file = new File(FILE_NAME);
+        } else {
+            file = new File(filePath, FILE_NAME);
+        }
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new IllegalArgumentException(filePath);
+            }
+        }
+        if(file.exists() && file.isFile()) {
+            try (FileWriter fileWriter = new FileWriter(file, true)) {
+                for(String line: lines) {
+                    fileWriter
+                            .append(currentDate + " ").append(line)
+                            .append(System.lineSeparator());
+                }
+            }
         }
     }
 }
